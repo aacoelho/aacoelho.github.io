05a82b07598f8311ff4a856315c9fe8f5a77c79f
Angela Coelho <aacoelho93@gmail.com>
2018-03-26 23:15:27 +0100

    Transition Effects

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ $(document).ready(function(){
     goTo("body");
   });
 
+  $(".link_transition").click(function(e){
+    $("body").css("overflow", "hidden");
+
+    e.preventDefault();
+    newLocation = this.href;
+
+    $(".main").addClass("fade-out");
+    $(".loading").removeClass("loading-out");
+    $(".loading").addClass("loading-in");
+    setTimeout(newpage, 300);
+
+  });
+
 });
 
 function goTo(target) {
@@ -15,4 +28,16 @@ function goTo(target) {
       scrollTop: $(target).offset().top
   }, 1500, "swing");
   return false;
+}
+
+$(window).on('load', function () {
+  $(".loading_text").css("opacity", "0");
+  $(".main").addClass("fade-in");
+  $(".loading").addClass("loading-out");
+  $("body").css("overflow", "visible");
+  //$(".loading").removeClass("loading-show");
+});
+
+function newpage() {
+  window.location = newLocation;
 }
