3a9e52f22483a848b528ddb0a915651b67b0b763
Ângela Coelho <angela@superhuit.ch>
2021-12-30 17:59:17 +0000

    Fix skew value

--- js/main.js
+++ js/main.js
@@ -1,81 +1,82 @@
 //Skew Effect Variables
 const section = $(".skewEffect");
 let currentPixel = $(window).scrollTop();
-const range = 15;
+const range = 5;
 
-$(document).ready(function(){
-
+$(document).ready(function () {
   //Arrows
-  $(".landingSection_corner .arrowSvg").on( "click", function() {
+  $(".landingSection_corner .arrowSvg").on("click", function () {
     goTo(".selectedWorks");
   });
 
-  $(".footer_arrow .arrowSvg").on( "click", function() {
+  $(".footer_arrow .arrowSvg").on("click", function () {
     goTo("body");
   });
 
   //Transition Out - Loading
-  $(".link_transition").click(function(e){
+  $(".link_transition").click(function (e) {
     e.preventDefault();
     newLocation = this.href;
 
     $("body").addClass("noScroll");
     $(".main").addClass("fade-out");
     $(".loading_text").css("opacity", "0");
-    $(".loading").removeClass("loading-hidden").removeClass("loading-out").addClass("loading-in");
+    $(".loading")
+      .removeClass("loading-hidden")
+      .removeClass("loading-out")
+      .addClass("loading-in");
     // $("body").css("overflow", "hidden");
     // $(".loading").removeClass("loading-out").removeClass("loading-hidden").addClass("loading-in");
 
-    setTimeout(function(){
-      $(location).attr('href', newLocation);
+    setTimeout(function () {
+      $(location).attr("href", newLocation);
     }, 300);
-
   });
 
   //Orientation Title
   orientationScreen();
 
-  $(window).resize(function(){
+  $(window).resize(function () {
     orientationScreen();
   });
 
-  $(window).on('orientationchange', function(event) {
+  $(window).on("orientationchange", function (event) {
     orientationScreen();
   });
 
   looper();
-
 });
 
-
 //Transition In - Loading
-$(window).on('load', function () {
-
+$(window).on("load", function () {
   $(".loading_text").css("opacity", "0");
   $(".loading").addClass("loading-out");
   $("body").removeClass("noScroll");
   $(".main").addClass("fade-in");
 
-  setTimeout(function(){
+  setTimeout(function () {
     $(".loading").addClass("loading-hidden");
   }, 300);
 });
 
 //Orientation Mobile Screen
-function orientationScreen(){
-  if(window.innerHeight < window.innerWidth){
+function orientationScreen() {
+  if (window.innerHeight < window.innerWidth) {
     $("body").addClass("landscape");
-  }
-  else {
+  } else {
     $("body").removeClass("landscape");
   }
 }
 
 //Scroll to Anchor
 function goTo(target) {
-  $("html, body").animate({
-      scrollTop: $(target).offset().top
-  }, 1500, "swing");
+  $("html, body").animate(
+    {
+      scrollTop: $(target).offset().top,
+    },
+    1500,
+    "swing"
+  );
   return false;
 }
 
@@ -83,23 +84,21 @@ function goTo(target) {
 const looper = function () {
   const newPixel = $(window).scrollTop();
   const diff = newPixel - currentPixel;
-  let speed = diff * 0.15;
+  let speed = diff * 0.12;
 
-  if(speed > range){
+  if (speed > range) {
     speed = range;
-  }
-  else if (speed < -range) {
+  } else if (speed < -range) {
     speed = -range;
   }
 
-  if(speed != 0){
+  if (speed != 0) {
     $(".skewEffect img").addClass("shadow");
-  }
-  else {
+  } else {
     $(".skewEffect img").removeClass("shadow");
   }
 
-  if($(window).width()<1024){
+  if ($(window).width() < 1024) {
     speed = 0;
   }
   section.css("transform", "skewY(" + speed + "deg)");
@@ -107,4 +106,4 @@ const looper = function () {
   currentPixel = newPixel;
 
   requestAnimationFrame(looper);
-}
+};
